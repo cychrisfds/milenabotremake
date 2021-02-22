f217f070f54bd1d5072c17192918db78140d6f56
キリスト教徒 <77716224+cychrisfds@users.noreply.github.com>
2021-02-22 04:33:51 -0300

    Add files via upload

---
+++ android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (akamebote)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (cychris)
+ * Copyright notice      (bot com suporte a Android/Java)
